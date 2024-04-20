4e250386ef528596f908ce87c3c77f2eb9cc50e7
sumit chand <sumitchandmale@gmail.com>
2024-04-20 18:13:33 +0530

    first commit

---
+++ SpeechRecog.py
@@ -0,0 +1,23 @@
+import speech_recognition as sr
+
+# Initialize the recognizer
+recognizer = sr.Recognizer()
+
+# Load an audio file
+audio_file = "harvard.wav"
+
+# Open the audio file
+with sr.AudioFile(audio_file) as source:
+    recognizer.adjust_for_ambient_noise(source)
+    audio_data = recognizer.record(source)
+
+
+    try:
+        # Recognize the speech
+        #text = recognizer.recognize_google(audio_data)
+        text =recognizer.recognize_whisper(audio_data)
+        print("Recognized speech: ", text)
+    except sr.UnknownValueError:
+        print("Speech recognition could not understand the audio.")
+    except sr.RequestError as e:
+        print(f"Could not request results from service; {e}")
